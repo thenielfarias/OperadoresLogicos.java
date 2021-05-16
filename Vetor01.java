0d794cccf0ca99cd6621c45df5e87a026d493ed5
Daniel Farias <dan.tfarias@gmail.com>
2021-05-16 18:57:56 -0300

    Add files via upload

---
+++ Vetor01.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class Vetor01 {
+    
+    public static void main(String[] args) {
+    
+        /*int n[] = {3,2,8,7,5,4};
+        for(int c=0; c<=n.length-1; c++){
+            System.out.println("Na posiçao " + c + " temos o valor " + n[c]); 
+        }*/
+
+        
+        /*String mes[] = {"Jan","Fev","Mar","Abr","Mai","Jun",
+            "Jul","Ago","Set","Out","Nov","Dez"};
+        int tot[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for(int c=0; c<mes.length; c++){
+            System.out.println("o mes de " + mes[c] + " tem " + 
+                tot[c] + " dias ao todo.");
+        }*/
+
+        
+        /*double v[] = {3.5, 2.75, 9, -4.5};
+        Arrays.sort(v);
+        for (double valor: v) {
+            System.out.print(valor + " ");
+        }*/
+
+        
+        /*int vet[] = {3, 7, 6, 1, 9, 4 ,2};
+        for(int v:vet) {
+            System.out.print(v + " ");
+        } 
+        System.out.println("");
+        int p = Arrays.binarySearch(vet, 1);
+        System.out.println("Encontrei o valor na posiçao " + p);*/
+ 
+        
+        /*int v[] = new int[20];
+        Arrays.fill(v, 0);
+        for(int valor: v){
+            System.out.print(valor + " ");
+        }*/    
+            
+    }
+}    
