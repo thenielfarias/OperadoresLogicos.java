359ee40713445afd5da1da0347b74e0ba5062061
Daniel Farias <dan.tfarias@gmail.com>
2021-05-16 16:18:25 -0300

    Add files via upload

---
+++ OperadorTernario.java
@@ -0,0 +1,13 @@
+
+public class OperadorTernario {
+  
+    public static void main(String args[]) {
+        
+        int n1, n2, r;
+        n1 = 14;
+        n2 = 18;
+        r = (n1>n2)?n1+n2:n2-n1;
+        
+        System.out.println(r);
+    }
+}
