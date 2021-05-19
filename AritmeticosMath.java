d5d20f120e0168c06b4d5af06d72dcbcf5c9ca8f
Daniel Farias <dan.tfarias@gmail.com>
2021-05-18 21:18:01 -0300

    Add files via upload

---
+++ AritmeticosMath.java
@@ -0,0 +1,28 @@
+
+public class AritmeticosMath {
+    
+    public static void main(String args[]) {
+       /*int n1 = 3;
+       int n2 = 5;
+       float m = (n1 + n2)/2;
+       System.out.println("A media e igual a " + m);*/
+    
+       /*int numero = 10;
+       int valor = 4 + numero--;
+       System.out.println(valor);*/
+        
+       /*int x = 4;
+       x += 2; // x = x + 2
+       System.out.println(x);*/
+       
+       /*float v = 8.9f;
+       int ar = (int) Math.round(v);
+       System.out.println(ar);*/
+       
+       
+       double ale = Math.random();
+       int n = (int) (5 + ale * (10-5));
+       System.out.println(n);
+       
+    }
+}
