bb139886310f5502b818502710b7062f862f90bf
Daniel Farias <dan.tfarias@gmail.com>
2021-05-18 21:24:50 -0300

    Add files via upload

---
+++ Repeticao01.java
@@ -0,0 +1,12 @@
+
+public class Repeticao01 {
+    
+    public static void main(String[] args) {
+        
+        int cc =0;
+        do {
+            System.out.println("Cambalhota" + cc);
+            cc++;                 
+        }   while (cc<4);        
+    }
+}
