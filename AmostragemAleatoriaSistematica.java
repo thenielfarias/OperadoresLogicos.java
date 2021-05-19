f0a9e6f6c3dc9f39b47ef87e1c2c93f7e44e5599
Daniel Farias <dan.tfarias@gmail.com>
2021-05-18 21:22:54 -0300

    Add files via upload

---
+++ AmostragemAleatoriaSistematica.java
@@ -0,0 +1,17 @@
+
+/**
+ * Um lojista adquiriu 1000 lâmpadas, mas não tem tempo para testar todas. Suponha que ele queira testar 2% delas por 
+ * amostragem aleatória sistemática e queira iniciar pela 26ª lâmpada. Dessa maneira, qual será a última lâmpada a ser testada?
+ */
+
+public class AmostragemAleatoriaSistematica {
+    
+    public static void main(String[] args) {    
+    int totlampadas =1000;
+    int amostra =(totlampadas/100*2);
+    int salto =(totlampadas/amostra);    
+        for(int l=26; l<=totlampadas; l+=salto) {
+            System.out.println(l);   
+        }            
+    }
+}
